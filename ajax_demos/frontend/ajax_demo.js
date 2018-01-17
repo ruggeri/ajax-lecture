29456c0d86edaeaec9d4c05bbc37f6457766d506
Ned Ruggeri <ruggeri@self-loop.com>
2018-01-16 23:08:13 -0800

    code up CatLikeToggle

--- ajax_demos/frontend/ajax_demo.js
+++ ajax_demos/frontend/ajax_demo.js
@@ -1,3 +1,7 @@
 const CatForm = require('./cat_form');
 const CatLikeToggle = require('./cat_like_toggle');
 const SearchableCatsList = require('./searchable_cats_list');
+
+$(document).ready(() =>{
+  $('button.cat-like-toggle').each((idx, el) => { new CatLikeToggle(el) });
+});
--- ajax_demos/frontend/cat_like_toggle.js
+++ ajax_demos/frontend/cat_like_toggle.js
@@ -1,9 +1,43 @@
-// 1. Element should have cat-id, like-state.
+// 1. Element should have cat-id, is-liked.
 // 2. Install clickHandler.
 // 3. Click handler should determine whether to DELETE or POST.
 // 4. Can use $el.prop('disabled', true) to disable temporarily.
 
 class CatLikeToggle {
+  constructor(el) {
+    this.$el = $(el);
+    this.catId = this.$el.data('cat-id');
+    this.isLiked = this.$el.data('is-liked');
+
+    this.$el.on('click', (e) => this.clickHandler(e));
+
+    this.render();
+  }
+
+  async clickHandler(e) {
+    e.preventDefault()
+
+    const method = this.isLiked ? 'DELETE' : 'POST';
+
+    this.$el.prop('disabled', true);
+    await $.ajax({
+      url: `/cats/${this.catId}/like`,
+      method: method,
+    });
+    this.$el.prop('disabled', false);
+
+    this.isLiked = !this.isLiked;
+
+    this.render()
+  }
+
+  render() {
+    if (this.isLiked) {
+      this.$el.text('Unlike!');
+    } else {
+      this.$el.text('Like!');
+    }
+  }
 }
 
 module.exports = CatLikeToggle;
