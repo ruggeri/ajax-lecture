01b1915105cd86ee5c1d09d64eeea4e84ef49c4e
Ned Ruggeri <ruggeri@self-loop.com>
2018-01-16 23:07:47 -0800

    fix LikesController

--- ajax_demos/app/controllers/likes_controller.rb
+++ ajax_demos/app/controllers/likes_controller.rb
@@ -1,21 +1,27 @@
 class LikesController < ApplicationController
   def create
+    sleep 1
+
     @like = Like.create!(
       user_id: current_user.id,
       cat_id: params[:cat_id]
     )
 
     respond_to do |format|
-      format.json { render 'show' }
+      format.json { render json: @like }
     end
   end
 
   def destroy
-    @like = Like.find(params[:id])
+    sleep 1
+
+    @like = Like.find_by(
+      user_id: current_user.id, cat_id: params[:cat_id]
+    )
     @like.destroy!
 
     respond_to do |format|
-      format.json { render 'show' }
+      format.json { render json: @like }
     end
   end
 end
