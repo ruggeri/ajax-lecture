2576feae1efd09ebe77d8baef8a4fb40c1750101
Ned Ruggeri <ruggeri@self-loop.com>
2018-01-16 17:35:30 -0800

    add some seeds; hard code current user

--- ajax_demos/db/seeds.rb
+++ ajax_demos/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.create!(
+  name: 'Markov',
+  personal_legend: 'scared of everything'
+)
+
+Cat.create!(
+  name: 'Curie',
+  personal_legend: 'cute but dangerous'
+)
+
+Cat.create!(
+  name: 'Breakfast',
+  personal_legend: 'possesses ancient wisdom'
+)
+
+User.create!(
+  username: 'ned'
+)
