30be962153d9d17ce1e78f4143629892ae6fe55d
Ned Ruggeri <ruggeri@self-loop.com>
2018-01-16 17:11:51 -0800

    initial checkin of ajax_demos project

---
+++ ajax_demos/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/ajax_demo.js",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  devtool: 'source-maps',
+};
